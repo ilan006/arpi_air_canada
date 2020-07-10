5aee60ac7d1b7bfb3b4350788d968f448eb86a57
rali-udem <critter@iro.umontreal.ca>
2020-07-10 16:10:10 -0400

    First commit for conversion from Excel to binary.

---
+++ import_excel.py
@@ -0,0 +1,28 @@
+"""This script converts and sanitizes the original Excel files to binary format."""
+import sys
+import pandas as pd
+import pickle
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: prog input_file output_file")
+        sys.exit(2)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    # open with pandas
+    print("Reading...", file=sys.stderr, end=' ')
+    data = [pd.read_excel(open(input_file, 'rb'), s, 0) for s in ['Defect Data', 'ATA CH-SEC',
+                                                                  'MEL Code Data', 'Trax Recurrent Data']]
+    print("done.", file=sys.stderr)
+
+    # write binary
+    print("Writing...", file=sys.stderr, end=' ')
+    pickle.dump(data, open(output_file, 'wb'))
+    print("done.", file=sys.stderr)
+
+
+if __name__ == '__main__':
+    main()
