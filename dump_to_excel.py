1ba96d3ed4a7311dee6783756dfaa22ea451006d
rali-udem <critter@iro.umontreal.ca>
2020-07-17 14:35:10 -0400

    Corrected status message.

--- dump_to_excel.py
+++ dump_to_excel.py
@@ -17,7 +17,7 @@ def main():
 
     [defect_df_train, defect_df_dev, defect_df_test, ata_df, mel_df, trax_df] = pickle.load(open(input_file, 'rb'))
 
-    print("Writing (this can take a few minutes) ...", file=sys.stderr, end=' ', flush=True)
+    print("Writing (this can take a few minutes)...", file=sys.stderr, end=' ', flush=True)
     with pd.ExcelWriter(output_file) as writer:
         defect_df_train.to_excel(writer, sheet_name='defect_train', index=False, freeze_panes=(1, 0))
         defect_df_dev.to_excel(writer, sheet_name='defect_dev', index=False, freeze_panes=(1, 0))
