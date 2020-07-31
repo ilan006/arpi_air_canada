0760e6ba5a4952e0c36b2ad519cac28ac43a38b1
rali-udem <critter@iro.umontreal.ca>
2020-07-31 10:31:20 -0400

    Created a dummy clusterer for demo.

--- sample_clusterer.py
+++ sample_clusterer.py
@@ -3,9 +3,14 @@ Sample program to show how to load the data and how to evaluate a (dummy) algori
 """
 import argparse
 import arpi_evaluator
+import numpy as np
 import pandas as pd
 import pickle
-import sys
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics.pairwise import pairwise_distances
+
+BEGINNING_OF_TIME = np.datetime64('1970-01-01T00:00:00')
+TIMEDELTA_HOUR = np.timedelta64(1, 'h')
 
 
 def main():
@@ -30,26 +35,27 @@ def main():
           f"{len(defect_df_dev.ac.unique())} in dev and {len(defect_df_test.ac.unique())} in test.")
     print(f"The 3rd defect text for dev : {defect_df_dev.iloc[2, defect_df_dev.columns.get_loc('defect_description')]}")
 
+    # show how to lookup a value in a dataframe
+    print("\n")
+    #TODO
+
+    #TODO: remove defects with null id, and reported_datetime that is empty
+
     # show how a dummy clusterer can be evaluated and further shows how pandas can be used
-    print("\nPredicting clusters.")
-    test_predictions = find_recurrent_defects(defect_df_test)
+    print("\nPredicting clusters...\n")
+    test_predictions = find_recurrent_defects_naively(defect_df_test)
 
     print("\nEvaluating clusters.")
     eval_results = arpi_evaluator.evaluate_recurrent_defects(defect_df_test, test_predictions)
 
 
-def find_recurrent_defects(defect_df):
+def find_recurrent_defects_naively(defect_df):
     """
-    Finds recurrent defects (dummy algorithm to show how to proceed with the data).
+    Finds recurrent defects (naive k-means algorithm to show how to proceed with the data).
 
     :param defect_df: The defect dataframe for which we try to find recurrent defects.
-    :return: A mapping between defect id and predicted cluster. The cluster None is used to indicate that the
-             defect is not recurrent.
+    :return:
     """
-
-    # we prepare the result datastructure, mapping a defect id to its cluster assignment, e.g.
-    # {'C-65335-1': 'blue', 'L-32159-1': 'red', 'C-12341-2': None} where None means that the defect is not recurrent
-    result = {}
 
     # we regroup defects by aircraft ('ac') since a given defect cannot be recurrent across different aircraft
     grouped_by_ac = defect_df.groupby('ac')
@@ -58,22 +64,61 @@ def find_recurrent_defects(defect_df):
     for name, ac_group in grouped_by_ac:
         print(f"Aircraft {name} has {len(ac_group)} defects reported.")
 
-        candidate_clusters = {}  # a map for our dymmy algorithm
+        labels = []  # we prepare the labels for each defect, in the order we encounter them
+        feature_matrix = np.zeros((len(ac_group), 2))  # we have only 2 features: chapter and timestamp
 
-        # we can then iterate over all rows of the data and use the fields we want.
-        # index is the row number and row is the data itself
-        for index, row in ac_group.iterrows():
+        # we can then iterate over all rows of the data and use the fields we want!
+        row_number = 0
+        for _, row in ac_group.iterrows():  # index is the row number and row is the data itself
             cur_type = row['defect_type']  # e.g. C, E or L
-            cur_chapter = row['chapter']  # the ATA chapter
+            cur_defect = row['defect']  # an int
+            cur_item = row['defect_item']  # an int
 
-            my_key = cur_type + '-' + cur_chapter
-            candidate_clusters[my_key] = candidate_clusters.get(my_key, set())
-            candidate_clusters[my_key].add(row)
+            cur_id = f'{cur_type}-{str(cur_defect)}-{str(cur_item)}'  # this uniquely identifies a defect
+            labels.append(cur_id)  # for our clustering algorithm (below)
 
+            if pd.notnull(row['chapter']):  # some values, like the ATA chapter here can be null (i.e. not available)
+                cur_chapter = row['chapter']
+            else:
+                cur_chapter = -1
 
+            cur_reported_date = row['reported_datetime']
 
+            # we convert the date to hours
+            cur_reported_hours = (cur_reported_date - BEGINNING_OF_TIME) // TIMEDELTA_HOUR
+
+            # we add the features to the feature matrix
+            feature_matrix[row_number] = (cur_chapter, cur_reported_hours)
+            row_number += 1
+
+        # our simple algorithm performs agglomerative clustering - this is not important for the purpose of this demo
+        clustering_model = AgglomerativeClustering(n_clusters=None, affinity='precomputed',
+                                                   distance_threshold=.1, linkage='average')
+        dist_matrix = pairwise_distances(feature_matrix, feature_matrix, metric=custom_distance_fun)
+        clusters = clustering_model.fit_predict(dist_matrix)
+
+        # we convert the clusters array([ 192,  192, 1193, ...,  247,  247,  357]) into a result data structure
+        cluster_map = {}  # a mapping from cluster name to list of defects
+        for i, cluster_label in enumerate(clusters):
+            cluster_map[cluster_label] = cluster_map.get(cluster_label, set())
+            cluster_map[cluster_label].add(labels[i])
+
+        result = list(cluster_map.values())
 
     return result
+
+
+def custom_distance_fun(defect1, defect2):
+    """This returns a simple distance function in interval [0,1] between 2 defects."""
+    distance = 1
+
+    # first check if they are within 30 days of each other
+    delta_days = abs(defect2[0] - defect1[0]) / 24
+    if delta_days <= 30:  # 30 days for our example
+        delta_chapter = 0 if defect1[1] == defect2[1] else 1
+        distance = 0.3 * (delta_days / 30.0) + 0.7 * delta_chapter
+
+    return distance
 
 
 if __name__ == '__main__':
