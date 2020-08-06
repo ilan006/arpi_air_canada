c87a2c1fb6769dbb46d4d1234a99b8f37d99cbdf
rali-udem <critter@iro.umontreal.ca>
2020-08-06 14:36:45 -0400

    Reorder columns.

--- combine_datasets.py
+++ combine_datasets.py
@@ -31,11 +31,12 @@ def main():
 
     print("Rearranging columns...", file=sys.stderr)
     proper_order = ['defect_type', 'defect', 'defect_item', 'defect_description', 'status', 'ac', 'reported_datetime',
-                    'chapter', 'section', 'paragraph', 'i_f_s_d',  'defect_category', 'mddr', 'defer', 'defer_date',
+                    'recurrent', 'chapter', 'section', 'paragraph', 'i_f_s_d',  'defect_category', 'mddr', 'defer',
+                    'defer_date',
                     'defer_hour', 'defer_minute', 'defer_to_date', 'defer_to_hour', 'defer_to_minute', 'mel',
                     'mel_number', 'reported_date', 'reported_hour', 'repoted_minute', 'resolution_description',
                     'resolution_category', 'resolved_date', 'resolved_hour', 'resolved_minute', 'created_date',
-                    'modified_date', 'recurrent', 'schedule_hours', 'schedule_cycles', 'schedule_days', 'skill',
+                    'modified_date', 'schedule_hours', 'schedule_cycles', 'schedule_days', 'skill',
                     'man_hours', 'man_required', 'sdr', 'repeat_number', 'completed_number', 'defer_notes',
                     'release_to_service_by', 'mel_calendar_days_flag', 'pn_incident', 'ongoing_trouble_shooting',
                     'planning_dept', 'planning_sub_dept', 'schedule_hours_repeat', 'schedule_cycles_repeat',
