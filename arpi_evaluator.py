e4f8399766e2c4dd5c8a54d9b2f0da2e1c1d1ff0
rali-udem <critter@iro.umontreal.ca>
2020-08-26 18:01:33 -0400

    Added additional verbose information.

--- arpi_evaluator.py
+++ arpi_evaluator.py
@@ -1,6 +1,7 @@
 """
 This script evaluates a candidate clustering given a reference.
 """
+import numpy
 import os
 
 from sklearn.metrics import homogeneity_completeness_v_measure
@@ -41,6 +42,7 @@ def evaluate_recurrent_defects(ref_df: pd.DataFrame, predictions, remove_ata_zer
         remove_ata_zero_section - copy of argument remove_ata_zero_section for this function
         nb_ref_clusters: nb of clusters in the reference
         nb_pred_clusters: nb of cluster predicted
+        avg_pred_cluster_size: average of predicted cluster size without any filtering
     """
 
     filled_df = ref_df.recurrent.fillna(NO_CLUSTER_LABEL)  # when there is no recurrent id, define as not clustered
@@ -68,7 +70,8 @@ def evaluate_recurrent_defects(ref_df: pd.DataFrame, predictions, remove_ata_zer
             'completeness': completeness, 'v_measure': v_measure_score,
             'pred_clusters': pred_clusters, 'ref_clusters': ref_clusters,
             'remove_ata_zero_section': remove_ata_zero_section,
-            'nb_ref_clusters': ref_clusters.nunique() - 1, 'nb_pred_clusters': len(set(pred_clusters)) }
+            'nb_ref_clusters': ref_clusters.nunique() - 1, 'nb_pred_clusters': len(set(pred_clusters)),
+            'avg_pred_cluster_size': numpy.mean([len(x) for x in predictions])}
 
 
 def convert_cluster_labels_to_seq(ref_df: pd.DataFrame, predictions):
